4da9b1610ae81cbd1b9b4da80cc9d1e932320410
cleitoncfs <cleitoncfs@gmail.com>
2024-12-10 12:38:02 +0000

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    plugins: [react()],
+    base: "/Lista-de-Livros/",
     build: {
-        outDir: "dist", // Define o diretório de saída como 'dist'
+        outDir: "build", // Define o diretório de saída como 'build'
     },
 });
