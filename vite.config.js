fd705f32298a641cd179f68d3bdcf1736adeb1af
cleitoncfs <cleitoncfs@gmail.com>
2024-12-10 14:09:15 +0000

    Adiciona configuração para copiar livros.json para build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "public/assets/livros.json", // Caminho do arquivo JSON
+                    dest: "assets", // Diretório de destino no build
+                },
+            ],
+        }),
+    ],
     base: "/Lista-de-Livros/",
     build: {
         outDir: "build", // Define o diretório de saída como 'build'
